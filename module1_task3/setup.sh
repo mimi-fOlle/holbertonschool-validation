59c16eab5594162996c5088ff04163ac8d496362
Yeh-Hsin LEE <oglanshi@gmail.com>
2023-01-04 17:52:17 +0100

    add comments in setup

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# Spawn a sandboxed environment with ubuntu 18.04
 docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
+
+# Install 'Hugo' and 'Make'
 apt-get update && apt-get install -y hugo make
+
+# Build the website
 make build
+
+# Exit
 exit
