ff20e1feacafb34670f375250657fa1b1f2641c9
Yeh-Hsin LEE <oglanshi@gmail.com>
2023-01-09 21:22:08 +0100

    update setup

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-
-# Update and install 'Hugo' and 'Make'
-sudo apt-get update && apt-get install -y hugo make
 
 # Download lastest release of 'Hugo'
 sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
@@ -12,6 +8,4 @@ sudo apt install ./last.deb
 
 sudo rm last.deb
 
-# Build the website
 make build
-
