e46286685ab590de16920881a8531e4fba861007
Yeh-Hsin LEE <oglanshi@gmail.com>
2023-01-09 18:32:17 +0100

    add sudo in setup

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -2,18 +2,16 @@
 docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
 
 # Update and install 'Hugo' and 'Make'
-apt-get update && apt-get install -y hugo make
-
-# Install 'curl'
-apt-get install curl -y
+sudo apt-get update && apt-get install -y hugo make
 
 # Download lastest release of 'Hugo'
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
 
 # Install the latest version of 'Hugo'
-apt-get install ./last.deb
+sudo apt install ./last.deb
 
-rm last.deb
+sudo rm last.deb
 
 # Build the website
 make build
+
