131e840ff4372c6b09897e57688ce6ccd71ec0e3
Yeh-Hsin LEE <oglanshi@gmail.com>
2023-01-10 18:42:03 +0100

    fix chekcer issue

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# Update and install 'Hugo' and 'Make'
-apt-get update && apt-get install -y make wget
-
 # Download lastest release of 'Hugo'
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
-tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb	
 
-mv hugo /usr/local/bin/
-rm hugo_extended_0.109.0_Linux-64bit.tar.gz
-
+sudo dpkg -i hugo_extended_0.109.0_linux-amd64.deb	
 # Install markdownlint and zip tools
 apt-get install zip -y
 npm install -g markdownlint-cli -y
-
-# Build the website
-make build
