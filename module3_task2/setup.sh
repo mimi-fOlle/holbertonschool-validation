e57286449d9a032f710d48946065e0af8806897f
Yeh-Hsin LEE <oglanshi@gmail.com>
2023-01-10 06:47:07 +0100

    try fix lint

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Update and install 'Hugo' and 'Make'
-sudo apt-get update && apt-get install -y hugo make shellcheck && npm install -g markdownlint-cli
+sudo apt-get update && apt-get install -y hugo make
 
 # Install 'curl'
 apt-get install curl -y
 
 # Download lastest release of 'Hugo'
-sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 
 # Install the latest version of 'Hugo'
-sudo apt install ./last.deb
+sudo apt install ./hugo.deb
 
-sudo rm last.deb
+sudo rm hugo.deb
 
 # Build the website
 make build
